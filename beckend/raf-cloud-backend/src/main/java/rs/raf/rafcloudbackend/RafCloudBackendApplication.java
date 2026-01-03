b908e90468ca2e95579bbf3445b41ecc73a04b3c
Kosta-D <kostadopudja@gmail.com>
2026-01-03 12:20:41 +0100

    Spring back proj

---
+++ beckend/raf-cloud-backend/src/main/java/rs/raf/rafcloudbackend/RafCloudBackendApplication.java
@@ -0,0 +1,13 @@
+package rs.raf.rafcloudbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafCloudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RafCloudBackendApplication.class, args);
+	}
+
+}
---
+++ beckend/raf-cloud-backend/src/test/java/rs/raf/rafcloudbackend/RafCloudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.raf.rafcloudbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RafCloudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
